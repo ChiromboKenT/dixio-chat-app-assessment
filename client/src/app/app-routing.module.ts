d4f3d4a7a8110408b8109ded5c0b38923df716be
Kenny Chirombo <57112312+ChiromboKenT@users.noreply.github.com>
2022-03-24 17:35:18 +0200

    added websockets to angular frontend

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from 'src/components/chat/chat.component';
+import { RegisterComponent } from 'src/components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: RegisterComponent,
+  },
+  {
+    path: 'chat',
+    component: ChatComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
